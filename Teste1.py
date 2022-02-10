a2a6419459bf4fdf9ca951b198ad7c0fa923e7bb
Pedro <pedrinhobryk17@gmail.com>
2022-02-10 15:28:43 -0300

    Projetos python cruso

---
+++ Teste1.py
@@ -0,0 +1,6 @@
+nome = input('Qual é seu nome?')
+idade = input('qual é sua idade?')
+peso = input('Qual é seu peso?')            
+print('Seu nome é:',nome,'sua idade é:',idade,'seu peso é:',peso)
+
+             
